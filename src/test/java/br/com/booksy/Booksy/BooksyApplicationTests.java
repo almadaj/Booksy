a56730821ae4989b25967aceebfeb62d5bae0d63
Juliano Almada <julianoalmada48@gmail.com>
2025-04-08 20:53:56 -0300

    Initial commit

---
+++ src/test/java/br/com/booksy/Booksy/BooksyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.booksy.Booksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
