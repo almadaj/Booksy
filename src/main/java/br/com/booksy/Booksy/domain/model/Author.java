240e21ba15aa6c6220860b83aa42f32148ab1ef2
Lucas Gomes <lucasgdo4@gmail.com>
2025-04-20 16:39:26 -0300

    test: adiciona testes unitários de AuthorRepository

--- src/main/java/br/com/booksy/Booksy/domain/model/Author.java
+++ src/main/java/br/com/booksy/Booksy/domain/model/Author.java
@@ -1,12 +1,18 @@
 package br.com.booksy.Booksy.domain.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "authors")
 public class Author {
     @Id
---
+++ src/test/java/br/com/booksy/Booksy/repository/AuthorRepositoryTest.java
@@ -0,0 +1,60 @@
+package br.com.booksy.Booksy.repository;
+
+import br.com.booksy.Booksy.domain.model.Author;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static br.com.booksy.Booksy.util.AuthorCreator.createAuthor;
+
+@DataJpaTest
+@DisplayName("Tests for Author Repository")
+class AuthorRepositoryTest {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Test
+    @DisplayName("Save persists author when successful")
+    void save_PersistsAuthor_WhenSuccessful() {
+        Author author = createAuthor();
+        Author savedAuthor = authorRepository.save(author);
+
+        Assertions.assertThat(savedAuthor).isNotNull();
+        Assertions.assertThat(savedAuthor.getId()).isNotNull();
+        Assertions.assertThat(savedAuthor.getFirstName()).isEqualTo(author.getFirstName());
+        Assertions.assertThat(savedAuthor.getLastName()).isEqualTo(author.getLastName());
+        Assertions.assertThat(savedAuthor.getOriginCountry()).isEqualTo(author.getOriginCountry());
+        Assertions.assertThat(savedAuthor.getBirthYear()).isEqualTo(author.getBirthYear());
+    }
+
+    @Test
+    @DisplayName("Save updates author when successful")
+    void save_UpdatesAuthor_WhenSuccessful() {
+        Author author = createAuthor();
+        Author savedAuthor = authorRepository.save(author);
+        savedAuthor.setFirstName("Steven");
+        savedAuthor.setLastName("Hawking");
+        savedAuthor.setOriginCountry("Brazil");
+        savedAuthor.setBirthYear(1945);
+        Author updatedAuthor = authorRepository.save(savedAuthor);
+
+        Assertions.assertThat(updatedAuthor).isNotNull();
+        Assertions.assertThat(updatedAuthor.getId()).isNotNull();
+        Assertions.assertThat(updatedAuthor.getFirstName()).isEqualTo(savedAuthor.getFirstName());
+        Assertions.assertThat(updatedAuthor.getLastName()).isEqualTo(savedAuthor.getLastName());
+        Assertions.assertThat(updatedAuthor.getOriginCountry()).isEqualTo(savedAuthor.getOriginCountry());
+        Assertions.assertThat(updatedAuthor.getBirthYear()).isEqualTo(savedAuthor.getBirthYear());
+    }
+
+    @Test
+    @DisplayName("Delete removes author when successful")
+    void delete_RemovesAuthor_WhenSuccessful() {
+        Author author = createAuthor();
+        Author savedAuthor = authorRepository.save(author);
+        authorRepository.delete(author);
+
+        Assertions.assertThat(authorRepository.findById(savedAuthor.getId())).isEmpty();
+    }
+}
