31e59083e76d15983fa8de072d85026b3720f1d6
Juliano Almada <julianoalmada48@gmail.com>
2025-04-20 18:46:28 -0300

    feat: adiciona as validaçoes necessárias em Review e Reading

--- src/main/java/br/com/booksy/Booksy/domain/model/Reading.java
+++ src/main/java/br/com/booksy/Booksy/domain/model/Reading.java
@@ -1,6 +1,8 @@
 package br.com.booksy.Booksy.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,21 +21,24 @@ public class Reading {
     @Column(columnDefinition = "uuid")
     private UUID id;
 
+//    @NotNull
 //    @Column(name = "userId", nullable = false)
 //    private UUID userId;
 
+//    @NotNull
 //    @Column(name = "bookId", nullable = false)
 //    private UUID bookId;
 
+    @Min(0)
     @Column()
     private int currentPage;
 
+    @NotNull
     @Column()
     private Date startDate;
 
     @Column()
     private Date endDate;
-
 
 
 }
--- src/main/java/br/com/booksy/Booksy/domain/model/Review.java
+++ src/main/java/br/com/booksy/Booksy/domain/model/Review.java
@@ -3,6 +3,7 @@ package br.com.booksy.Booksy.domain.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.Date;
 import java.util.UUID;
@@ -11,7 +12,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "rating")
+@Table(name = "review")
 public class Review {
     @Id
     @GeneratedValue
@@ -25,16 +26,21 @@ public class Review {
 //    private UUID bookId;
 
     @Column()
+    @NotNull
     @Min(1)
     @Max(5)
     private int rating;
 
+    @NotNull
     @Column(length = 100)
     private String title;
 
-    @Column
+    @NotNull
+    @Size(max = 2000)
+    @Column(length = 2000)
     private String textPost;
 
+    @NotNull
     @Column
     private Date postDate;
 
